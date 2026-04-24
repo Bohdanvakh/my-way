2545e0742ab84b086d1f13a9ee75b74da9cbf679
bohdanvakh <bohdanvakh@gmail.com>
2026-04-25 00:54:46 +0300

    Add Post model.

---
+++ prisma/migrations/20260424214911_added_posts_model/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Made the column `firstName` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `lastName` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "firstName" SET NOT NULL,
+ALTER COLUMN "lastName" SET NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "approved" BOOLEAN NOT NULL,
+    "published" BOOLEAN NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
