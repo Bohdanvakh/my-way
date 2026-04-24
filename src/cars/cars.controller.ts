7ea6b887614b5045991bfc9ba0f03847dda80b4e
bohdanvakh <bohdanvakh@gmail.com>
2026-04-25 00:57:09 +0300

    Created my onw endpoint.

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -17,6 +17,11 @@ export class CarsController {
     return this.carsService.findAll();
   }
 
+  @Get('newer')
+  findNewest() {
+    return this.carsService.findNewer();
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.carsService.findOne(+id);
