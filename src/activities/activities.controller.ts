1775d32640da84d3e459e8a9ed3ca8b25b5afb9f
bohdanvakh <bohdanvakh@gmail.com>
2026-05-27 00:29:18 +0300

    feat/add update and findOne methods

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, HttpCode } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { CreateActivityDto } from './dto/create-activity.dto';
+import { UpdateActivityDto } from './dto/update-activity.dto';
 
 @Controller('activities')
 export class ActivitiesController {
     constructor(private activitiesService: ActivitiesService) {}
+
+    @Get(':id')
+    @HttpCode(200)
+    findOne(@Param('id') id: string) {
+        return this.activitiesService.findOne(Number(id));
+    }
 
     @Get()
     @HttpCode(200)
@@ -16,5 +23,11 @@ export class ActivitiesController {
     @HttpCode(201)
     create(@Body() createActivityDto: CreateActivityDto) {
         return this.activitiesService.create(createActivityDto);
+    }
+
+    @Patch(':id')
+    @HttpCode(201)
+    update(@Param('id') id: string, @Body() updateActivityDto: UpdateActivityDto ) {
+        return this.activitiesService.update(Number(id), updateActivityDto);
     }
 }
